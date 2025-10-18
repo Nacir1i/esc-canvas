00ed29d4256917ee9d43f914c2d84ceca0909345
Naciri Taoufiq <taoufiqnaciri234@gmail.com>
2025-10-18 19:47:20 +0100

    create renderer function to visualize hitboxes

--- src/libs/Renderer.ts
+++ src/libs/Renderer.ts
@@ -28,20 +28,21 @@ export class Renderer {
     this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
     this.renderFrame(scene);
+    this.renderHitboxes(scene);
   }
 
   private renderFrame(scene: Scene) {
     const assetEntities = scene.query([
+      "State",
       "Transform",
       "Dimensions",
       "AnimationCollection",
-      "State",
     ]);
 
     for (const entityId of assetEntities) {
       const entityState = scene.componentMaps.State.get(entityId)!;
-      // const entityDimensions = scene.componentMaps.Dimensions.get(entityId)!;
       const entityTransform = scene.componentMaps.Transform.get(entityId)!;
+      const entityDimensions = scene.componentMaps.Dimensions.get(entityId)!;
       const entityAnimations =
         scene.componentMaps.AnimationCollection.get(entityId)!;
 
@@ -57,9 +58,44 @@ export class Renderer {
         animationFrame.spriteHeight,
         entityTransform.x,
         entityTransform.y,
-        animationFrame.spriteWidth,
-        animationFrame.spriteHeight
+        entityDimensions.width,
+        entityDimensions.height
       );
+    }
+  }
+
+  private renderHitboxes(scene: Scene) {
+    const hitboxEntities = scene.query(["Hitbox"]);
+
+    const patternCanvas = document.createElement("canvas");
+    const patternCtx = patternCanvas.getContext("2d")!;
+
+    const patternSize = 10;
+    patternCanvas.width = patternSize;
+    patternCanvas.height = patternSize;
+
+    patternCtx.strokeStyle = "#99ff00ff";
+    patternCtx.lineWidth = 2;
+    patternCtx.beginPath();
+    patternCtx.moveTo(0, 0);
+    patternCtx.lineTo(patternSize, patternSize);
+    patternCtx.stroke();
+
+    for (const entityId of hitboxEntities) {
+      const entityHitbox = scene.componentMaps.Hitbox.get(entityId)!;
+
+      const fillPattern = this.canvasContext.createPattern(
+        patternCanvas,
+        "repeat"
+      )!;
+
+      const rectX = entityHitbox.x;
+      const rectY = entityHitbox.y;
+      const rectWidth = entityHitbox.width;
+      const rectHeight = entityHitbox.height;
+
+      this.canvasContext.fillStyle = fillPattern;
+      this.canvasContext.fillRect(rectX, rectY, rectWidth, rectHeight);
     }
   }
 }
