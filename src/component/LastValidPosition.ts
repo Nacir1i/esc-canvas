e6f2a94c91247281ac643396b8c5237d55dd8156
Naciri Taoufiq <taoufiqnaciri234@gmail.com>
2025-11-01 22:29:32 +0100

    new components

---
+++ src/component/LastValidPosition.ts
@@ -0,0 +1,9 @@
+export class LastValidPosition {
+  public x: number;
+  public y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
---
+++ src/component/Text.ts
@@ -0,0 +1,11 @@
+export class Text {
+  color: string;
+  font: string;
+  content: string;
+
+  constructor(color: string, font: string, content: string) {
+    this.color = color;
+    this.font = font;
+    this.content = content;
+  }
+}
