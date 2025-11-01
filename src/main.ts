a9d046a55bcf4b09dbd869f91636173967c2bd2d
Naciri Taoufiq <taoufiqnaciri234@gmail.com>
2025-11-01 22:30:21 +0100

    update main function

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,34 @@ import "./style.css";
 
 import { Loop } from "./libs/Loop";
 import { Scene } from "./libs/Scene";
+import { createOrb } from "./entity/orb";
 import { Renderer } from "./libs/Renderer";
 import { movement } from "./system/movement";
 import { animations } from "./system/animations";
-import { InputManager } from "./libs/InputManager";
+import { createGhostEntity } from "./entity/ghost";
 import { createPlayerEntity } from "./entity/player";
-import { AssetsManager } from "./libs/AssetsManager";
+import { SpriteSheetLoader } from "./libs/SpriteSheetLoader";
+import { createMapBorders } from "./entity/mapBorders";
 import { playerMovement } from "./system/playerMovement";
+import { createPlayerScore } from "./entity/playerScore";
 import { movementDumping } from "./system/movementDumping";
 import { movementAnimation } from "./system/movementAnimation";
+import { updatePlayerScore } from "./system/updatePlayerScore";
+import { collisionDetection } from "./system/collisionDetection";
+import { playerOrbCollision } from "./system/playerOrbCollision";
+import { handleBlockingCollisions } from "./system/handleBlockingCollisions";
 import { syncHitboxPositionToEntityPosition } from "./system/syncHitboxPositionToEntityPosition";
 
 export const CANVAS_WIDTH = 900;
 export const CANVAS_HEIGHT = 900;
 
 const player1StartingPosition = {
-  x: 10,
-  y: 10,
+  x: 20,
+  y: 20,
+};
+const ghostStartingPosition = {
+  x: 100,
+  y: 100,
 };
 
 async function main() {
@@ -31,12 +42,11 @@ async function main() {
   const canvasContext = canvas.getContext("2d");
   if (!canvasContext) throw new Error("Missing canvas context");
 
-  const assetsManager = new AssetsManager();
+  const assetsManager = new SpriteSheetLoader();
 
   await assetsManager.loadSprite();
 
-  const inputManager = new InputManager();
-  const scene = new Scene(inputManager);
+  const scene = new Scene();
   const renderer = new Renderer(
     CANVAS_WIDTH,
     CANVAS_HEIGHT,
@@ -45,24 +55,39 @@ async function main() {
   );
 
   const player = createPlayerEntity(player1StartingPosition);
+  const ghost = createGhostEntity(ghostStartingPosition);
+  const mapBorders = createMapBorders(CANVAS_WIDTH, CANVAS_HEIGHT);
+  const orb = createOrb(300, 300);
+  const orb2 = createOrb(310, 310);
+  const orb3 = createOrb(320, 320);
+  const orb4 = createOrb(330, 330);
+  const playerScore = createPlayerScore();
 
   scene.registerSystem(playerMovement);
-  scene.registerSystem(movementDumping);
   scene.registerSystem(movement);
   scene.registerSystem(syncHitboxPositionToEntityPosition);
+  scene.registerSystem(collisionDetection);
+  scene.registerSystem(playerOrbCollision);
+  scene.registerSystem(handleBlockingCollisions);
   scene.registerSystem(movementAnimation);
   scene.registerSystem(animations);
+  scene.registerSystem(movementDumping);
+  scene.registerSystem(updatePlayerScore);
 
-  scene.registerComponent(player.id, "State", player.state);
-  scene.registerComponent(player.id, "Hitbox", player.hitbox);
-  scene.registerComponent(player.id, "Velocity", player.velocity);
-  scene.registerComponent(player.id, "Transform", player.transform);
-  scene.registerComponent(player.id, "Dimensions", player.dimensions);
-  scene.registerComponent(player.id, "AnimationCollection", player.animations);
+  mapBorders.forEach((border) => {
+    scene.registerComponents(border.id, border.components);
+  });
+  scene.registerComponents(player.id, player.components);
+  scene.registerComponents(ghost.id, ghost.components);
+  scene.registerComponents(orb.id, orb.components);
+  scene.registerComponents(orb2.id, orb2.components);
+  scene.registerComponents(orb3.id, orb3.components);
+  scene.registerComponents(orb4.id, orb4.components);
+  scene.registerComponents(playerScore.id, playerScore.components);
 
   const loop = new Loop(scene, renderer);
 
-  inputManager.attachListeners();
+  scene.inputManager.attachListeners();
   loop.start();
 }
 
